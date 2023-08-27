87c226d09abd1133e30166ae2e34da630ec31303
Palok kumar mandol <139862084+Palok123@users.noreply.github.com>
2023-08-27 20:56:04 +0600

    Add files via upload

---
+++ CashRegister.java
@@ -0,0 +1,30 @@
+package Capter8;
+
+public class CashRegister {
+    private double purchase;
+    private double payment;
+    public CashRegister()
+    {
+        purchase = 0;
+        payment = 0;
+    }
+    public void recordPurchase(double amount)
+    {
+        purchase = purchase + amount;
+    }
+    public void recievePayment(int coinCount, Coin coinType)
+    {
+        payment = payment + coinCount * coinType.getCoinValue();
+    }
+    public double getTotalChange()
+    {
+        return (payment - purchase);
+    }
+    public double giveChange()
+    {
+        double change = payment - purchase;
+        purchase = 0;
+        payment = 0;
+        return change;
+    }
+}
